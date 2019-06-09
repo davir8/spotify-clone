541233e99021b3b872f8590cc0cb016a56f66e6e
Davi <davirodrigues8@gmail.com>
2019-06-09 17:39:23 -0300

    adds browse page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
+
 import { Wrapper, Container, Content } from './styles/components';
 
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import Header from './components/Header';
 
+import Routes from './routes';
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Wrapper>
         <Container>
           <Sidebar />
           <Content>
             <Header />
+            <Routes />
           </Content>
         </Container>
         <Player />
       </Wrapper>
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
 
