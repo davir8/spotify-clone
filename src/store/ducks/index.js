48a386cb8ecf6146975bbb2821d8f5e5a03b94b5
Davi <davirodrigues8@gmail.com>
2019-06-14 13:05:38 -0300

    adds errors validate

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 
 import playlists from './playlists';
 import playlistDetails from './playlistDetails';
+import error from './error';
 
-export default combineReducers({ playlists, playlistDetails });
+export default combineReducers({ playlists, playlistDetails, error });
